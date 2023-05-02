5b807d3c01733c6bf91f47f62dc94bb04ed68495
Luis C <me@lucataco.com>
2023-05-01 23:42:52 -0400

    Fix Vercel Analytics for tsx app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import { Analytics } from'@vercel/analytics/react';
-import '../styles/globals.css';
+import { Analytics } from "../components/Analytics";
+import "../styles/globals.css";
 
 export default function RootLayout({
   children,

fd257f071b1b16039bf86b1d4a475d9f62f5ef11
Luis Catacora <me@lucataco.com>
2025-09-01 21:55:48 -0400

    Cleanup JS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
-import { Analytics } from "../components/Analytics";
 import "../styles/globals.css";
 
-type LayoutProps = {
-  children: React.ReactNode;
-};
+type LayoutProps = { children: any };
 
-const Layout: React.FC<LayoutProps> = ({ children }) => (
+const Layout = ({ children }: LayoutProps) => (
   <html lang="en">
     <head></head>
     <body>
       {children}
-      <Analytics />
       <script defer src="https://data.lucataco.dev/script.js" data-website-id="b13bd10a-0079-4345-a654-ad02cc54b3b7"></script>
     </body>
   </html>
